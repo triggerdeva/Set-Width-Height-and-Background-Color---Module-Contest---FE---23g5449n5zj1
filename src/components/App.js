ec2e3564e17645c7cb050603ab2fdff1a3ef980a
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-04-16 09:53:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
 
 const App = () => {
   const [width, setWidth] = useState("");
   const [height, setHeight] = useState("");
   const [backgroundColor, setBackgroundColor] = useState("");
+  const [boxes, setBoxes] = useState([]);
+
+  const handleWidthChange = (event) => {
+    setWidth(event.target.value);
+  };
+
+  const handleHeightChange = (event) => {
+    setHeight(event.target.value);
+  };
+
+  const handleColorChange = (event) => {
+    setBackgroundColor(event.target.value);
+  };
+
+  const handleCreateBox = () => {
+    const newBox = {
+      width,
+      height,
+      backgroundColor,
+    };
+    setBoxes([...boxes, newBox]);
+    setWidth("");
+    setHeight("");
+    setBackgroundColor("");
+  };
 
   return (
     <div id="main">
@@ -14,6 +39,8 @@ const App = () => {
         <input
           id="width"
           type="number"
+          value={width}
+          onChange={handleWidthChange}
         />
       </div>
       <div>
@@ -21,6 +48,8 @@ const App = () => {
         <input
           id="height"
           type="number"
+          value={height}
+          onChange={handleHeightChange}
         />
       </div>
       <div>
@@ -28,13 +57,26 @@ const App = () => {
         <input
           id="backgroundColor"
           type="text"
+          value={backgroundColor}
+          onChange={handleColorChange}
         />
       </div>
-      <button className='create-btn'>Create Box</button>
-      <div id="box-container"></div>
+      <button className='create-btn' onClick={handleCreateBox}>Create Box</button>
+      <div id="box-container">
+        {boxes.map((box, index) => (
+          <div
+            key={index}
+            style={{
+              width: `${box.width}px`,
+              height: `${box.height}px`,
+              backgroundColor: box.backgroundColor,
+              margin: "10px",
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 };
-
 
 export default App;
